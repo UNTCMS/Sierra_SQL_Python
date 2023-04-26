b96262292f0860a765b83ec80b52dee4c63ab482
UNT Libraries, Cataloging and Metadata Services <96254577+UNTCMS@users.noreply.github.com>
2023-04-26 10:01:10 -0500

    Add files via upload

---
+++ 08 3XX Subfields/345 Moving Image Characteristics/8_4_5-Bad345_dAspectRatioDesignation.py
@@ -0,0 +1,46 @@
+import psycopg2
+import csv
+from postgresql_password import postgresql_password
+
+report = 'I:/SQL Queries/Code verification working files/8_4_5-Bad345_dAspectRatioDesignation.csv'
+
+query = """
+SELECT
+	r.record_type_code || r.record_num || 'a' AS "Record Number",
+	s.marc_tag AS "MARC Tag",
+	s.marc_ind1 AS "Ind1",
+	s.marc_ind2 AS "Ind2",
+	s.tag AS "Subfield",
+	s.content AS "Content"
+FROM
+	sierra_view.record_metadata r
+	JOIN sierra_view.subfield s ON s.record_id = r.id
+		AND s.marc_tag = '345'
+		AND s.tag = 'd'
+WHERE
+	r.record_type_code = 'b'
+	AND s.content NOT IN (
+		'full screen',
+		'mixed aspect ratio',
+		'wide screen',
+-- Uncontrolled terms
+		'anamorphic wide screen')
+--ORDER BY r.record_num ASC;
+ORDER BY s.content ASC;
+    """
+
+conn = psycopg2.connect(host="sierra-db.library.unt.edu", port="1032", database="iii", user="swolf", password=postgresql_password)
+
+cur = conn.cursor()
+cur.execute(query)
+
+report_data = cur.fetchall()
+
+with open(report, 'w', newline='') as csvfile:
+    rd = csv.writer(csvfile)
+  #  rd.writerow(['Record Number', 'MARC Tag', 'Ind1', 'Ind2', 'Content'])
+    for row in report_data:
+        rd.writerow(row)
+
+print('csv file generation complete')
+
