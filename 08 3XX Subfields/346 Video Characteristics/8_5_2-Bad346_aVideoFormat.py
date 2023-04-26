c7367e854e6a92dbc068d622ece087ed0818d7ee
UNT Libraries, Cataloging and Metadata Services <96254577+UNTCMS@users.noreply.github.com>
2023-04-26 10:01:41 -0500

    Add files via upload

---
+++ 08 3XX Subfields/346 Video Characteristics/8_5_2-Bad346_aVideoFormat.py
@@ -0,0 +1,56 @@
+import psycopg2
+import csv
+from postgresql_password import postgresql_password
+
+report = 'I:/SQL Queries/Code verification working files/8_5_2-Bad346_aVideoFormat.csv'
+
+query = """
+SELECT
+	r.record_type_code || r.record_num || 'a' AS "Record Number",
+	s.marc_tag AS "MARC Tag",
+	s.marc_ind1 AS "Ind1",
+	s.marc_ind2 AS "Ind2",
+	s.tag AS "Subfield",
+	s.content AS "Content"
+FROM
+	sierra_view.record_metadata r
+	JOIN sierra_view.subfield s ON s.record_id = r.id
+		AND s.marc_tag = '346'
+		AND s.tag = 'a'
+WHERE
+	r.record_type_code = 'b'
+	AND s.content NOT IN (
+		'8 mm',
+		'Betacam',
+		'Betacam SP',
+		'Betamax',
+		'CED',
+		'D-2',
+		'EIAJ',
+		'Hi-8 mm',
+		'laser optical',
+		'M-II',
+		'Quadruplex',
+		'Super-VHS',
+		'Type C',
+		'U-matic',
+		'VHS')
+--ORDER BY r.record_num ASC;
+ORDER BY s.content ASC;
+    """
+
+conn = psycopg2.connect(host="sierra-db.library.unt.edu", port="1032", database="iii", user="swolf", password=postgresql_password)
+
+cur = conn.cursor()
+cur.execute(query)
+
+report_data = cur.fetchall()
+
+with open(report, 'w', newline='') as csvfile:
+    rd = csv.writer(csvfile)
+  #  rd.writerow(['Record Number', 'MARC Tag', 'Ind1', 'Ind2', 'Content'])
+    for row in report_data:
+        rd.writerow(row)
+
+print('csv file generation complete')
+
